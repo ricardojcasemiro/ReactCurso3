8b4aedd1f8e5408bffef4c711cbd76140424ac3c
ricardo.casemiro <ricardo.casemiro@kcsitpt.onmicrosoft.com>
2021-04-27 08:55:29 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# cautious-funicular
+# react-complete-guide
 Projetos em react ref. ao Curso Udemy react-complete-guide
+
+Curso:  https://www.udemy.com/course/react-the-complete-guide-incl-redux/?LSNPUBID=JVFxdTr9V80&ranEAID=JVFxdTr9V80&ranMID=39197&ranSiteID=JVFxdTr9V80-7iUIQFDWONKyDZDfwRkdAQ&utm_medium=udemyads&utm_source=aff-campaign
+
+# Para rodas o projeto
+1- abra seu gitBash
+2- Depois execute o comando:
+	git clone https://github.com/ricardojcasemiro/ReactCurso3.git
+3- na pasta do prjeto aind no seu git bash digite:
+	code .
+4- Depois de abrir o visual code abra um terminal e digite:
+	npm install
+	npm start
---
+++ src/01-starting-setup/src/components/Expenses/Expenses.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import ExpenseList from './ExpenseList';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return (
+      (expense.date.getFullYear().toString() === filteredYear) ||
+      (filteredYear === 'All')
+      );
+  });
+
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter 
+          selected={filteredYear} 
+          onChangeFilter={filterChangeHandler} 
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpenseList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
